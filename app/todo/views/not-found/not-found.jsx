05d77fe1bb70fca65b1bc13e625d672aa41a65ed
Serge Droganov <serge@droganov.ru>
2016-03-02 01:12:02 +0000

    proper style formating

--- app/todo/views/not-found/not-found.jsx
+++ app/todo/views/not-found/not-found.jsx
@@ -2,11 +2,13 @@
 import React, { Component } from "react";
 import Helmet from "react-helmet";
 
+import styles from "./not-found.styl"
+
 export default class NotFound extends Component {
    static propTypes = {};
    render() {
       return (
-         <div className="NotFound">
+         <div className={ styles[ "not-found" ] }>
             <Helmet title="Page not found" />
             <h1>404</h1>
             <p>Not found</p>
